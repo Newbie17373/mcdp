24275a1561670a6e0435bf2488fa45fd0ef3e8a3
Bakerman <danil.metelkin.2003@mail.ru>
2024-01-31 00:08:29 +0600

    add some new logic for nodeJS/express server

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const port = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
 app.listen(port, (error) => {
-	error ? console.log(error) : console.log(`listening port ${port}`);
+  error ? console.log(error) : console.log(`listening port ${port}`);
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(createPath('index'));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+console.log(path.join(__dirname, "assets"));
+app.use(express.static(path.join(__dirname, "assets")));
+
+app.get("/", (req, res) => {
+  const title = "index";
+  res.render(createPath("index"), { title });
 });
 
-app.get('/contacts', (req, res) => {
-	res.sendFile(createPath('index'));
+app.get("/contacts", (req, res) => {
+  const title = "contacts";
+  res.render(createPath("index"), { title });
 });
 
-app.get('/index.html', (req, res) => {
-	res.redirect('/');
+app.get("/index.html", (req, res) => {
+  res.redirect("/");
 });
 
-app.get('/index.html', (req, res) => {
-	res.redirect('/');
+app.get("/index.html", (req, res) => {
+  res.redirect("/");
 });
 
 app.use((req, res) => {
-	res.status(404).sendFile(createPath('error'));
+  const title = "error";
+  res.status(404).render(createPath("error"), { title });
 });
